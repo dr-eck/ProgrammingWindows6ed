78f4809e61043fdb3bc0a52c1a75bfec2c1a00a5
Steve Eckhardt <steve@eckop.com>
2023-11-02 12:08:28 -0500

    incorporated Raymond Chen's suggestion to change the return type of OnButtonClick to "winrt::fire_and_forget". This allows all of the code to be in that function.

--- CppWinRT/Ch7/HowToAsync1/HowToAsync1/MainWindow.xaml.cpp
+++ CppWinRT/Ch7/HowToAsync1/HowToAsync1/MainWindow.xaml.cpp
@@ -18,32 +18,28 @@ using namespace Microsoft::UI::Xaml::Navigation;
 
 namespace winrt::HowToAsync1::implementation
 {
-    IAsyncAction ShowDlg(Grid contentGrid, Button myButton)
+    winrt::fire_and_forget MainWindow::OnButtonClick(IInspectable const&, RoutedEventArgs const&)
     {
+        //auto rv{ ShowDlg(contentGrid(), myButton()) };
         ContentDialog dialog;
         dialog.Title(box_value(L"title"));
         dialog.Content(box_value(L"content"));
         dialog.PrimaryButtonText(L"Red");
         dialog.SecondaryButtonText(L"Blue");
         dialog.CloseButtonText(L"Close");
-        dialog.XamlRoot(myButton.XamlRoot());
+        dialog.XamlRoot(myButton().XamlRoot());
 
         auto cdr = co_await dialog.ShowAsync();
 
         if (cdr == ContentDialogResult::Primary)
         {
             SolidColorBrush myBrush{ Microsoft::UI::Colors::Red() };
-            contentGrid.Background(myBrush);
+            contentGrid().Background(myBrush);
         }
         else if (cdr == ContentDialogResult::Secondary)
         {
             SolidColorBrush myBrush{ Microsoft::UI::Colors::Blue() };
-            contentGrid.Background(myBrush);
+            contentGrid().Background(myBrush);
         }
-    }
-
-    void MainWindow::OnButtonClick(IInspectable const&, RoutedEventArgs const&)
-    {
-        auto rv{ ShowDlg(contentGrid(), myButton()) };
     }
 }
--- CppWinRT/Ch7/HowToAsync1/HowToAsync1/MainWindow.xaml.h
+++ CppWinRT/Ch7/HowToAsync1/HowToAsync1/MainWindow.xaml.h
@@ -13,7 +13,17 @@ namespace winrt::HowToAsync1::implementation
             // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
         }
 
-        void OnButtonClick(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        // docs: https://learn.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/concurrency-2#fire-and-forget
+        // Specifically, "winrt::fire_and_forget is also useful as the return type of your event handler 
+        // when you need to perform asynchronous operations in it."
+        // someone on StackOverflow mentioned that fire_and_forget is like async void in C#
+        // Also from StackOverflow (https://stackoverflow.com/questions/65593996/winrtfire-and-forget-what-does-this-do)
+        // Apart from documentation for Fire and forget, note the following: the function itself uses co_await operator in its body.
+        // This requires that the function itself is coroutine friendly and could be compiled into "stackless" form, 
+        // for asynchronous execution. void return type does not work out, but fire_and_forget struct is okay because
+        //  C++ / WinRT defines coroutine handling for it, as explained by the documentation.
+        // Think of this as void which can be asynchronous and has no need to be waited on.
+        winrt::fire_and_forget OnButtonClick(IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
     };
 }
 
